504eddc6c51bb4a9833f4024317c580e730fa2c5
Murilo Lobos <murilolobos47@gmail.com>
2021-05-17 23:11:17 -0300

    install.sh done

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+cd
+git clone https://github.com/laximit/multi
+cd multi
 sudo chmod a+x multicode
 sudo cp multicode /bin
+cd ..
+rm -rf multi
 echo "Done, now you can call the script by simply typing 'multicode'."
